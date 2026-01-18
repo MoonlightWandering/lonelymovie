ec5a3e511c4532e85de43a35ae02192c1fe65b9c
K7! <phoenex659@gmail.com>
2026-01-18 06:00:44 +0530

    fix: use relative API paths and enable global CORS for Render deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import VideoPlayer from './VideoPlayer'
 
-const API_URL = 'http://127.0.0.1:8000'
+const API_URL = ''
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('')
